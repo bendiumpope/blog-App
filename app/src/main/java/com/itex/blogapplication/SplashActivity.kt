39bb89e74fa4178cb7df39661efbe69e4348b2e3
benedict <bendiumpope@gmail.com>
2019-12-30 20:48:38 +0100

    Create Splash_screen and Login UI

---
+++ app/src/main/java/com/itex/blogapplication/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.itex.blogapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+
+    private val SPLASH_TIME_OUT= 3000L
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+
+        Handler().postDelayed({
+            val intent = Intent(this@SplashActivity, LoginActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right)
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
